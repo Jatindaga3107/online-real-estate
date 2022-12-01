76e52d059157e91e4bdac0080219ba7ec3b30a09
The boss hacker jaga jasus <119155785+Jatindaga3107@users.noreply.github.com>
2022-12-01 20:53:13 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,8 @@
+const hamburer = document.querySelector(".hamburger");
+const navList = document.querySelector(".nav-list");
+
+if (hamburer) {
+  hamburer.addEventListener("click", () => {
+    navList.classList.toggle("open");
+  });
+}
---
+++ slider.js
@@ -0,0 +1,19 @@
+const swiper = new Swiper(".heroslider", {
+    spaceBetween: 30,
+    slidesPerView: 1,
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    effect: "fade",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+  });
